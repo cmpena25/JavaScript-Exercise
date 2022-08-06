62481e79577ffe0db14c6f682615da22ac5b38aa
celeste pena <cmpena25@gmail.com>
2022-08-06 18:56:13 -0500

    operators, typeof, arithmtic, string concatenation, logical boolean, and comparisons

--- js/external.js
+++ js/external.js
@@ -3,3 +3,23 @@ console.log(3 + 5);
 console.log(typeof 42);
 console.log(5 % 2);
 console.log('Hello,' + ' World!');
+console.log(false && false || true); // true
+console.log(false && (false || true)); // false
+console.log('1' + 2); // 12
+console.log(typeof '1' + 2); // 'string2'
+console.log(6 % 4); // 2
+console.log('3 + 4 is ' + (3 + 4)); // '3 + 4 is 7'
+console.log(0 < 0); // false
+console.log('false' == false); // false
+console.log(true == 'true'); // false
+console.log(5 >= - 5); // true
+console.log(!false || false); // true
+console.log(true || "42"); // true
+console.log(!true && !false); // false
+console.log(6 % 5); // 1
+console.log(5 < 4 && 1 === 1); // false
+console.log(typeof 'codeup' === 'string'); // true
+console.log('codeup' === 'codeup' && 'codeup' === 'Codeup'); // false
+console.log(4 >= 0 && 1 !== '1'); // true
+console.log(6 % 3 === 0); // true
+console.log(5 % 2 !== 0); // true
