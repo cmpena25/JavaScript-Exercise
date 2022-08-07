4a07c6b521d98aa2cbf6d51966433fb29dc0d25c
celeste pena <cmpena25@gmail.com>
2022-08-06 22:03:01 -0500

    shorthand and unary operators

---
+++ js/dataTypesLesson.js
@@ -0,0 +1,25 @@
+// shorthand
+var x = 2;
+x += 10;
+console.log(x); // 12
+
+var y = 3;
+y /= 3;
+console.log(y); // 1
+
+// unary operators
+var z = 1;
+++z;
+console.log(z); // 2
+
+z = 2;
+--z;
+console.log(z); // 1
+console.log(z--);
+console.log(z);
+
+
+
+
+
+
