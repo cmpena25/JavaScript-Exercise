46b02f80f8d5cd8e2af91c80eefc0f3596fd9aad
celeste pena <cmpena25@gmail.com>
2022-08-07 13:31:50 -0500

    javascript built in methods

--- js/dataTypesLesson.js
+++ js/dataTypesLesson.js
@@ -34,6 +34,26 @@ console.log(isNaN('abc')); // true
 console.log(isNaN(42)); // false
 console.log(isNaN(Number('abc'))); // true
 
+// parseInt and parseFloat functions
+console.log(parseInt(Number('123abc'))); // NaN
+console.log(Number('123abc')); // NaN
+console.log(Number('123')); // 123
+console.log(parseInt('123abc')); // 123
+console.log(parseFloat("    40.00 h   ")); //40
+
+
+// useful javascript functions
+var givenName = 'Celeste';
+console.log(givenName.length); // 7
+console.log(givenName.indexOf('l')); // 2
+console.log(givenName.replace('C', 'S')); // Seleste
+console.log(givenName.substring(0, 3)); // Cel
+console.log(givenName.toUpperCase()); // CELESTE
+console.log(givenName.toLowerCase()); // celeste
+
+var newName = '  Celeste   ';
+console.log(newName);
+console.log(newName.trim());
 
 
 
