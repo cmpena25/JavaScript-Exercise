95066415a3950e8a9c50d515bb4cdcd2a9b2e27e
celeste pena <cmpena25@gmail.com>
2022-08-07 20:31:40 -0500

    working with variables

---
+++ js/variables-lessons.js
@@ -0,0 +1,24 @@
+var name = "Celeste";
+console.log(name); // Celeste
+
+var differentName = "Michelle" , number = 25;
+console.log(differentName, number); // Celeste 25
+
+const pi = 3.145;
+console.log(pi); // 3.145 and cannot be reassigned
+
+var n = 10 -2 * 8 +16 / 2;
+console.log(n); // 2
+
+var x = 5;
+var y = x + 3;
+console.log(y); // 8
+
+x = 7;
+console.log(x); // 7
+
+var x = 5;
+x = x + 3;
+console.log(x); // 8
+
+
