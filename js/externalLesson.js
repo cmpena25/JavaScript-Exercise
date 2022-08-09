bb36bef18b8d3e6084f832369c4a9e839855c513
celeste pena <cmpena25@gmail.com>
2022-08-08 21:16:46 -0500

    testing alert, confirm, prompt functions

--- js/externalLesson.js
+++ js/externalLesson.js
@@ -1,5 +1,15 @@
 alert('Welcome to my website!');
 
 var userInput = prompt('What is your favorite color?');
-console.log(alert('Great,' + userInput + ' is my favorite color too!'));
+console.log(alert('Great, ' + userInput + ' is my favorite color too!'));
+
+alert('You must login to view the rest of the website!');
+var confirmed = confirm('Your username must be no more than 20 characters');
+console.log(confirmed);
+var username = prompt('Enter your username');
+console.log(alert(username));
+alert('1.Your password must be at least 5 characters' + ' 2.Your password must not include the username.' + ' 3.neither the username or password can start or end with whitespace');
+var password = prompt('Enter your Password');
+
+
 
