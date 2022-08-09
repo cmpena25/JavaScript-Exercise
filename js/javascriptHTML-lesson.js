24ac9d02542fc9f1fed51eb7e2a7d2c8f53ab882
celeste pena <cmpena25@gmail.com>
2022-08-08 20:25:38 -0500

    user interaction, alert, confirm, and prompt functions

---
+++ js/javascriptHTML-lesson.js
@@ -0,0 +1,15 @@
+// debugging a console
+console.log('Hello from Javascript!');
+
+
+"use strict";
+ var x = 25;
+console.log(x);
+
+alert('Hello, World!');
+
+var confirmed = confirm('Is this the correct website?');
+console.log(confirmed);
+
+var userInput = prompt('Type in your name.');
+console.log('The user entered:' +userInput);
