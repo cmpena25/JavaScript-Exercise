7663dd6f8c1e55dee142e5b0aefe0640c0446a3f
celeste pena <cmpena25@gmail.com>
2022-11-12 15:07:43 -0600

    local and gloval variables

--- js/functionsLesson.js
+++ js/functionsLesson.js
@@ -11,4 +11,48 @@ var five = addition(2, 3);
 console.log(five);
 
 
+var fortyTwo = '42';
+console.log(parseInt(fortyTwo));
 
+console.log(typeof fortyTwo);
+
+function increment(x){
+    return x + 1;
+}
+
+var six = increment(5);
+console.log(six);
+
+
+var decrement = function(x) {
+    return x - 1;
+}
+
+var eight = decrement(9);
+console.log(eight);
+
+// global var
+
+var globalVar = "Look, I'm Global!";
+function sayHello() {
+    alert(globalVar);
+}
+
+sayHello();
+
+function sayMyName(name) {
+    alert("Hello " + name.toUpperCase());
+}
+
+sayMyName("celeste");
+
+// local var
+
+function scope() {
+    var localVar ="Look, I'm Local!";
+    alert(localVar);
+    alert(globalVar);
+}
+
+scope();
+ // cannot be used in an alert
