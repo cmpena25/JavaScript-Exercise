fef6f64f2abf168ad8f0d22abe2ed4aec364dfa2
celeste pena <cmpena25@gmail.com>
2022-11-12 14:25:35 -0600

    created a function that added the sum of two parameters

---
+++ js/functionsLesson.js
@@ -0,0 +1,14 @@
+function addition(x , y) {
+    return x + y;
+}
+var three = addition(1, 2);
+console.log(three);
+
+var four = addition(2, 2);
+console.log(four);
+
+var five = addition(2, 3);
+console.log(five);
+
+
+
